0b6dec6063e3f105fbc0d7f60f990b443e496693
sreeharika <sreeharikayelle@gmail.com>
2020-03-22 03:56:11 +0530

    state updated

---
+++ src/components/Board.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Square from './Square'
+
+export default class Board extends Component {
+    renderSquare(i){
+    return <Square value={i}></Square>
+    }
+    render() {
+        const status ='Nextplayer:X';
+        return (
+            <div>
+            <div className='status'>{status}</div>
+            <div className='boarder-row'>
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </div>
+            <div className='border-row'>
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            </div>
+            <div className='border-row'>
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            </div>
+            </div>
+        )
+    }
+}
